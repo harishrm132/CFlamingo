e7e3df3e982548645f12c3f342e3748a179fa4ee
Harish <harishrm132@gmail.com>
2021-09-05 18:30:21 +0530

    Add project files.

---
+++ CFlamingo/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace CFlamingo
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event fireeed when child property changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
